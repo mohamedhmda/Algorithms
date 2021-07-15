2d740bcd962e726f6bab35d41a46d602feb89cd3
Mohamed Hamaida <mohamed.amine.hamaida@gmail.com>
2021-07-15 18:14:03 +0100

    15 jul 2021| run lenght encoding

---
+++ run_lenght_encoding/run_lenght_encoding.py
@@ -0,0 +1,17 @@
+def run_lenght_encoding(string):
+    lenght = len(string)
+    new_string = ""
+    i = 0
+    while i < lenght:
+        char = string[i]
+        count = 0
+        while i < lenght and string[i] == char:
+            i += 1
+            count += 1
+        new_string = ''.join([new_string,char,str(count)])
+
+
+    return new_string    
+    
+
+print(run_lenght_encoding("abcd"))
---
+++ run_lenght_encoding/test.py
@@ -0,0 +1,20 @@
+import unittest
+
+target = __import__('run_lenght_encoding')
+
+class TestRunLenghtEncoding(unittest.TestCase):
+    def test_case_1(self):
+        string = "wwwwaaadexxxxxx"
+        new_string = "w4a3d1e1x6"
+        result = target.run_lenght_encoding(string)
+        self.assertEqual(result, new_string)
+
+    def test_case_2(self):
+        string = "abcd"
+        new_string = "a1b1c1d1"
+        result = target.run_lenght_encoding(string)
+        self.assertEqual(result, new_string)
+    
+
+if __name__ == '__main__':
+    unittest.main()
